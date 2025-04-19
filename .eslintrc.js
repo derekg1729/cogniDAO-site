0d0f3bc4f7a0c520713830d8ca6ed77f145a6101
derekg1729 <derekgranito@gmail.com>
2025-04-19 00:48:16 -0700

    fix(lint): configure ESLint properly for edge detection checks and next/core

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
+  extends: ['next/core-web-vitals'],
   plugins: ['custom-rules'],
   rules: {
-    'custom-rules/no-edge-runtime': 'warn'
+    'custom-rules/no-edge-runtime': 'error'
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')]
+    }
   }
 } 
